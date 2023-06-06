6e19ec758ef8e4535f478d6bb005e176ec7f9229
Ralph Royeen A. Lagumen <rralagumen@unionbankph.com>
2023-06-06 20:19:54 +0800

    Initial commit

---
+++ src/main/java/com/esemes/core/EsemesCoreApplication.java
@@ -0,0 +1,13 @@
+package com.esemes.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsemesCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsemesCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esemes/core/EsemesCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esemes.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsemesCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
